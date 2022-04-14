7a90d84f72cf15dbec7369146fc24130891d54a8
Samarth Saxena <SamarthSaxena1672003@gmail.com>
2022-04-14 19:01:03 +0530

    updated

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,3 +1,3 @@
 const WHITELIST_CONTRACT_ADDRESS="0x2494DdDFe71A5086deb5f5188F08C46F45dB8a22"
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-awesamarth.vercel.app/api/";
 module.exports={WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
